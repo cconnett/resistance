66578b1663a94f6bbe8263f3295afd9bf916f755
Chris Connett <chris@connett.net>
2013-02-09 23:52:41 -0500

    Move window.CLOSURE_NO_DEPS to be inserted by build script.

--- ripscript.sh
+++ ripscript.sh
@@ -1,4 +1,10 @@
-sed -e '/goog\/base.js/ d' -e '/js\/resistance.js/ {
+sed -e '/goog\/base.js/ {
+a  <script>
+a    window.CLOSURE_NO_DEPS=true;
+a  </script>
+d
+}' \
+    -e '/js\/resistance.js/ {
 i <script>
 r js.js
 a </script>
